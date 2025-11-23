1345bb9771e43d0f01b72a76583ec598f195ec06
HenryTene <henry_tene@hotmail.com>
2025-11-23 11:40:37 -0500

    :sparkles: crear API Resource para categorías

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -4,16 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\Categoria;
 use Illuminate\Http\Request;
+use App\Http\Resources\CategoriaCollection;
 
 class CategoriaController extends Controller
 {
     public function index()
     {
-        return response()->json([
-            'status' => 'success',
-            'data' => [
-                'categorias' => Categoria::all()
-            ]
-        ]);
+        return new CategoriaCollection(Categoria::all());
     }
 }
