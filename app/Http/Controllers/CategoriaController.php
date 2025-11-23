ed75ab8a7e7d00e164daa385504cf0041c82e106
HenryTene <henry_tene@hotmail.com>
2025-11-22 22:14:57 -0500

    :sparkles: crear respuesta JSON para categorías

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categoria;
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json([
+            'status' => 'success',
+            'data' => [
+                'categorias' => Categoria::all()
+            ]
+        ]);
+    }
 }
